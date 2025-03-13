4b7eb98702cf291649ffe0637687e4d4037bd64d
abheesree <csree8@gmail.com>
2025-03-13 18:51:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ mongoose.connect(mongodb)
 
 
 const app = express();
-app.get('/',(req,res)=>res.send('Hello world 13 march app'));
+app.get('/',(req,res)=>res.send('Hello world 13 march app 6:50'));
 
 const port = process.env.PORT || 4000;
 
